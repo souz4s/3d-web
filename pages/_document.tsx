40188c6006089db38816175fffc47c9a4f6eee56
souz4s <psouza.1650@gmail.com>
2022-04-19 20:54:44 -0300

    icon import adjustment

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -45,8 +45,6 @@ export default class MyDocument extends Document {
             <Html lang="pt-br">
                 <Head>
                     <meta charSet="utf-8" />
-                    <link rel="android-chrome" sizes="512x512" href="images/android-chrome-192x192.png" />
-                    <link rel="android-chrome" sizes="512x512" href="images/android-chrome-512x512.png" />
                     <link rel="apple-touch-icon" sizes="180x180" href="images/apple-touch-icon.png" />
                     <link rel="icon" type="image/png" sizes="32x32" href="images/favicon-32x32.png" />
                     <link rel="icon" type="image/png" sizes="16x16" href="images/favicon-16x16.png" />
