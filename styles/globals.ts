af7dbe79c6e3fd3b46a9d85184b1d9ed3de2323f
souz4s <psouza.1650@gmail.com>
2022-04-19 21:06:16 -0300

    css reset

--- styles/globals.ts
+++ styles/globals.ts
@@ -1,13 +1,12 @@
 import { createGlobalStyle } from "styled-components";
 
 export default createGlobalStyle`
-  * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
-
+  html,
   body {
     margin: 0;
+	  padding: 0;
+	  border: 0;
+	  vertical-align: baseline;
+    list-style: none;
   }
 `
