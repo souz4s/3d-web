26cd18312b3977ec5ae282645e63ddf22f677c63
souz4s <psouza.1650@gmail.com>
2022-05-26 18:38:53 -0300

    imported style

--- pages/components/Cursor.tsx
+++ pages/components/Cursor.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef } from "react";
-import styled from "styled-components";
+import { DotOutline, Dot } from "../../styles/cursor";
 
 const runOnClient = (func: () => any) => {
     if (typeof window !== "undefined") {
@@ -12,30 +12,6 @@ const runOnClient = (func: () => any) => {
 };
 
 const Cursor: React.FC = () => {
-
-    const DotOutline = styled.div`
-        width: 40px;
-        height: 40px;
-        background: linear-gradient(61.69deg, #EB5757 13.74%, #F2994A 88.78%);;
-        pointer-events: none;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-        border-radius: 50%;
-        opacity: 1;
-        transition: opacity 0.3s ease-in-out, transform 0.3s ease-in-out;
-    `;
-
-    const Dot = styled.div`
-        pointer-events: none;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-        opacity: 0;
-    `;
-
     const dot = useRef(null);
     const dotOutline = useRef(null);
 
